5eeff76ea73292cb5cfc621c9a3f330b6322dc03
andrzejboran <borowiak.and@gmail.com>
2016-11-24 12:20:06 +0100

    zapisywanie odpowiedzi urzytkownika

--- app/src/main/java/com/example/andrzej/quiz/QuestionActivity.java
+++ app/src/main/java/com/example/andrzej/quiz/QuestionActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -26,6 +27,7 @@ public class QuestionActivity extends AppCompatActivity {
 
     private int mCurrentQuestion = 0;
     private List<Question> mQuestions;
+    private int[] mAnswersArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,22 +36,31 @@ public class QuestionActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         mQuestions = (List<Question>) getIntent().getSerializableExtra("questions");
+        mAnswersArray = new int[mQuestions.size()];
         refreshQuestionView();
     }
 
     private void refreshQuestionView() {
+        mAnswers.clearCheck();
         Question q1 = mQuestions.get(mCurrentQuestion);
         mTitle.setText(q1.getContent());
         for (int i = 0; i < 3; i++) {
             mAnswersButtons.get(i).setText(q1.getAnswers().get(i));
+        }
+        //Sprawdza czy dla danego pytania zostałą udzielona odpowiedź, jeśli tak to zaznacza wybraną
+        if (mAnswersArray[mCurrentQuestion] > 0) {
+            mAnswers.check(mAnswersArray[mCurrentQuestion]);
         }
     }
 
     @OnClick(R.id.btn_back)
     protected void onBackClick() {
         if (mCurrentQuestion == 0) {
+            finish();
             return;
         }
+        //Zapisanie udzielonej odpowiedzi na aktualne pytanie
+        mAnswersArray[mCurrentQuestion] = mAnswers.getCheckedRadioButtonId();
         mCurrentQuestion--;
         refreshQuestionView();
     }
@@ -57,6 +68,11 @@ public class QuestionActivity extends AppCompatActivity {
     @OnClick(R.id.btn_next)
     protected void onNextClick() {
         if (mCurrentQuestion == mQuestions.size() - 1) {
+            return;
+        }
+        mAnswersArray[mCurrentQuestion] = mAnswers.getCheckedRadioButtonId();
+        if (mAnswersArray[mCurrentQuestion] == -1) {
+            Toast.makeText(this, "Musisz udzielić odpowiedzi", Toast.LENGTH_SHORT).show();
             return;
         }
         mCurrentQuestion++;
